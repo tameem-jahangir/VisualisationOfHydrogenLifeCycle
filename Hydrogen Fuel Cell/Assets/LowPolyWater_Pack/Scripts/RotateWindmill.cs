96ab8527dd98047bae5308c79db29c7843ea0d86
tameem.j <tameem.jahangir@gmail.com>
2022-03-08 14:39:29 +0100

    Updates to Landscapes and new animations for windmills

---
+++ Hydrogen Fuel Cell/Assets/LowPolyWater_Pack/Scripts/RotateWindmill.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWindmill : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f,0f,200f)*Time.deltaTime);
+    }
+}
